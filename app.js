4691ee0da21606280b674f10b25b90282c5184c8
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-01 19:17:14 +0900

    Lesson288 List function and add CSS file

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 const port = 3000
 
 app.listen(port, () => console.log(`Express server listening on port ${port}.`))
 
+let items = ['test 1', 'test 2', 'test 3']
+
 app.get('/', (req, res) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const today = days[(new Date()).getDay()]
-    res.render('list', { today })
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    const today = (new Date()).toLocaleDateString('en-CA', options)
+    res.render('list', { today, items })
+})
+
+app.post('/', (req, res) => {
+    items.push(req.body.newItem)
+    res.redirect('/')
 })
