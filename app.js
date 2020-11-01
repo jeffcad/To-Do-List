0f48cc58deb7764e04b19e0733f73309c9e2de9c
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-01 15:19:00 +0900

    Lesson285 Add EJS, basic template test

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+app.set('view engine', 'ejs')
 
 const port = 3000
 
 app.listen(port, () => console.log(`Express server listening on port ${port}.`))
 
 app.get('/', (req, res) => {
-    res.send('Hello, Mr. Thompson.')
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const today = days[(new Date()).getDay()]
+    res.render('list', { today })
 })
